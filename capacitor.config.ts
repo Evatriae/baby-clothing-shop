60607463e9d70b2c566e73fd05383680fd12de34
John Godfrey Malig <maligjohn4@gmail.com>
2025-10-01 13:50:17 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'baby-clothing-shop',
+  webDir: 'www'
+};
+
+export default config;
