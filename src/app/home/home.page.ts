9257a3fbf68c300ef8b1dc65e150af6133ea68e1
John Godfrey Malig <maligjohn4@gmail.com>
2025-10-01 14:23:38 +0800

    commit 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgIf, NgFor } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonButtons,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon,
+  IonList,
+  IonItem
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { menuOutline, personCircleOutline, basketOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    NgIf,
+    NgFor,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonButtons,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonIcon,
+    IonList,
+    IonItem
+  ],
 })
 export class HomePage {
-  constructor() {}
+  showAccordion = false;
+
+  constructor() {
+    addIcons({ menuOutline, personCircleOutline, basketOutline });
+  }
+
+  featuredCategories = [
+    { name: 'Pink with Flowers', img: 'assets/M-1.png' },
+    { name: 'White and Pink', img: 'assets/162198a6-d981-4ff8-8008-74545cc053cf.png' },
+    { name: 'Harlow Sets', img: 'assets/usoEdK.png' },
+    { name: 'New Trends', img: 'assets/Smith-and-Wesson-MP-1.png' },
+  ];
+
+  bestSellers = [
+    { name: 'Chloe Set', img: 'assets/108861-DEFAULT-l.png' },
+    { name: 'Romper with Stripes', img: 'assets/98879-DEFAULT-l.png' },
+    { name: 'Pink Dress', img: 'assets/stainless-raptor-9mm_2.png' },
+    { name: 'Short Sleeve', img: 'assets/66127e1e1ddc0_large.png' },
+    { name: 'Flower Sets', img: 'assets/receiver_pump-jpg-1.png' },
+    { name: 'Romper with Hearts', img: 'assets/K-1.png' }
+  ];
+
+  toggleAccordion() {
+    this.showAccordion = !this.showAccordion;
+  }
 }
